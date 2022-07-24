08ed6b7a28d840d566795997ba7731a6fdea2839
Harry Ying <lexugeyky@outlook.com>
2022-07-24 17:33:17 +0800

    change keywind keyloak upstream to flibrary

--- pkgs/keywind-theme.nix
+++ pkgs/keywind-theme.nix
@@ -5,10 +5,14 @@ stdenv.mkDerivation rec {
   version = "git";
 
   src = fetchFromGitHub {
-    owner = "lukin";
+    # owner = "lukin";
+    # repo = "keywind";
+    # rev = "224d73742ed4176920fb0c1e61f16752f6c1064c";
+    # sha256 = "sha256-VvpaR1bVJdjZAee05eVqWBiifWB34YYfwD9pnLVqm3M=";
+    owner = "flibrary";
     repo = "keywind";
-    rev = "224d73742ed4176920fb0c1e61f16752f6c1064c";
-    sha256 = "sha256-VvpaR1bVJdjZAee05eVqWBiifWB34YYfwD9pnLVqm3M=";
+    rev = "32093b06987a6e44a53ac2bf7301e448a52729ee";
+    sha256 = "sha256-fhjRW1qgqwfM5Yr73HJXLh1ZESIy1epKVFFlcbkUKsQ=";
   };
 
   installPhase = ''
