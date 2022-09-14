770c151a44c1f280ab3ed962f21479856cb3b520
Harry Ying <lexugeyky@outlook.com>
2022-09-14 10:08:43 +0800

    Update keywind

--- pkgs/keywind-theme.nix
+++ pkgs/keywind-theme.nix
@@ -1,18 +1,18 @@
 { lib, stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
-  pname = "keywind-thme";
+  pname = "keywind-theme";
   version = "git";
 
   src = fetchFromGitHub {
-    owner = "lukin";
-    repo = "keywind";
-    rev = "224d73742ed4176920fb0c1e61f16752f6c1064c";
-    sha256 = "sha256-VvpaR1bVJdjZAee05eVqWBiifWB34YYfwD9pnLVqm3M=";
-    # owner = "flibrary";
+    # owner = "lukin";
     # repo = "keywind";
-    # rev = "32093b06987a6e44a53ac2bf7301e448a52729ee";
-    # sha256 = "sha256-fhjRW1qgqwfM5Yr73HJXLh1ZESIy1epKVFFlcbkUKsQ=";
+    # rev = "224d73742ed4176920fb0c1e61f16752f6c1064c";
+    # sha256 = "sha256-VvpaR1bVJdjZAee05eVqWBiifWB34YYfwD9pnLVqm3M=";
+    owner = "flibrary";
+    repo = "keywind";
+    rev = "4b05a9b7c36876e65e69f767acd235fdb218f978";
+    sha256 = "sha256-zjRdKPnomvQiGDK+i+q/p8eeOtZkOMurnvp6jWXCPAs=";
   };
 
   installPhase = ''
